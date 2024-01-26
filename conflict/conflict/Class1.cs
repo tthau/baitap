de9eabf58bc0f00d5f6362740d04d977e54b7d7a
hau <tthau2004bd77@gmail.com>
2024-01-26 16:41:32 +0700

    thong bao

---
+++ conflict/conflict/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conflict
+{
+    internal class Class1
+    {
+        int x = 2;
+        int y = 3;
+        int z = 4;
+        int a;
+        int b;
+    }
+}
